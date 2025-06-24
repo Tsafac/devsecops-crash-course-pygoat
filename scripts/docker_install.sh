62981705bae9e26471c36ee8804e7a200800d1d6
Tsafac <ronicefabrice@gmail.com>
2025-06-24 23:07:04 +0200

    fix dast

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -2,7 +2,7 @@
 
 echo "Install using the apt repository"
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -13,7 +13,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
 
 echo "docker package"
@@ -23,10 +23,10 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 
 echo "Install Docker Desktop"
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install ./docker-desktop-amd64.deb
 
 echo " verification"
 
 docker --version
-docker compose version
+
